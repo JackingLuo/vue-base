af604bcace429a42864bce4f8e8b9e9385246ef2
jiaxin luo <jiaxinluo@jiaxindeMacBook-Pro.local>
2021-06-01 18:15:55 +0800

    提交vue配置文件

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+/**
+ * Vue的相关配置
+ */
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://test.com',
+//         ws: true,
+//         changeOrigin: true,
+//         pathRewrite: {
+//           '^/api': '/',
+//         }
+//       }
+//     }
+//   }
+// }
